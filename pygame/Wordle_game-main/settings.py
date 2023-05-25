0aafa347ddcbe142ad5dc85e7ff603102e8ab8cf
Vishal Singhania <knownstranger10@gmail.com>
2023-05-25 22:52:55 +0530

    updated

---
+++ pygame/Wordle_game-main/settings.py
@@ -0,0 +1,23 @@
+# COLORS (r, g, b)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+GREEN = (0, 200, 0)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+YELLOW = (200, 200, 0)
+BGCOLOUR = DARKGREY
+
+# game settings
+WIDTH = 400
+HEIGHT = 500
+FPS = 60
+title = "Wordle"
+icon = "assets/Icon.png"
+
+TILESIZE = 60
+GAPSIZE = 10
+
+MARGIN_X = int((WIDTH - (5 * (TILESIZE + GAPSIZE))) / 2)
+MARGIN_Y = int((HEIGHT - (6 * (TILESIZE + GAPSIZE))) / 2)
